77f44ad1ba130619a921486abdfea29ef2cdf756
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-11-03 19:45:27 +0200

    Perform first and second steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
-import { Heading } from 'components';
+import { Header, Loader } from 'components';
+import { lazy, Suspense, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getUserInfo } from 'service/opencagedataApi';
+
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+      getUserInfo(pos.coords);
+
+      console.log('Ваше текущее местоположение:');
+      console.log(`Широта: ${crd.latitude}`);
+      console.log(`Долгота: ${crd.longitude}`);
+      console.log(`Плюс-минус ${crd.accuracy} метров.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rates" element={<Rates />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
